06c2610048e0367dec415b10465c1f246d74c3da
Koushik Naskar <koushik.naskar9@gmail.com>
2022-08-23 10:30:44 +0530

    update

--- bot.py
+++ bot.py
@@ -6,6 +6,10 @@ import asyncio
 from bs4 import BeautifulSoup #used to parse websites
 import telebot
 from aiohttp import ClientSession
+import requests # used to fetch websites
+
+headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36' }
+
 
 TOKEN = os.getenv("TOKEN")
 ADMIN = os.getenv('ADMIN')
@@ -39,9 +43,9 @@ class DataBase:
     def addItem(self,user,link):
         # try:
         print('adding new iterm')
-        name, price = queryPrice([link])[0]
+        # name, price = queryPrice([link])[0]
+        name,price = check_price_flipkart(link)
         print('details of the item',name,price,user)
-        # bot.send_message(user,'fuyfuyff')
         priceInPaisa = price
         try:
             priceInPaisa = int(price)*100
@@ -54,7 +58,7 @@ class DataBase:
                 cur.execute('INSERT into ITEMS (userId, link, name,price) values (%s,%s,%s,%s) ',(user,link,name,priceInPaisa))
                 print('Inserted into database')
 
-                bot.send_message(user,f'<i> {name}</i> is added for tracking. Current price: <b> {price} </b>')
+                bot.send_message(user,f'<i> {name}</i> is added for tracking. \nCurrent price: <b> {price} </b>')
                 print('sending message')
         # except Exception as e:
         #     bot.send_message(user,"Failed to add the link. Check if its a proper link")
@@ -109,6 +113,25 @@ async def auxqueryPrice(URLs):
 
 def queryPrice(URLs):
     return asyncio.run(auxqueryPrice(URLs))
+
+
+
+
+def check_price_flipkart(url:str):
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    print('checking price for the link',url)
+    if 'flipkart' in url: # if flipkart
+        title = soup.find("span", {"class": "B_NuCI"}).get_text()
+        price = soup.find("div", {"class": "_30jeq3 _16Jk6d"}).get_text()[1:].replace(',','')
+    
+    elif 'amazon' in url or 'amzn' in url:# for amazon
+        title = soup.find("span", {"id": "productTitle"}).get_text()
+        price = soup.find("span", {"class": "a-offscreen"}).get_text()[1:].replace(',','')
+    print('queried successfully',price,title) #prints the price
+    return title,price
+
+
 
 
 
