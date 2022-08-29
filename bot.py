a23259f06c384c5b4a9b5c755c472596bab3d1c7
Koushik Naskar <koushik.naskar9@gmail.com>
2022-08-29 14:33:19 +0530

    update

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 import os
-from flask import Flask, request
-from psycopg2 import connect
-import asyncio
-from bs4 import BeautifulSoup #used to parse websites
+import re
 import telebot
-from aiohttp import ClientSession
-from aiohttp.client_exceptions import InvalidURL
-import requests # used to fetch websites
+import asyncio
 import traceback
 from time import sleep
 from threading import Timer
+from bs4 import BeautifulSoup 
+from flask import Flask, request
+from aiohttp import ClientSession
+from psycopg2 import connect, InterfaceError
+from aiohttp.client_exceptions import InvalidURL
+import requests 
+
 
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36' }
 
@@ -43,16 +45,40 @@ class DataBase:
     def connectToDb(self):
         # flyio postgresql disconnects after 30 minutes of inactivity
         if self.con.closed !=0:
+            sendToAdmin(f'Current value of connection {self.con.closed}')
             self.con = connect(self.dbFile)
             sleep(1)
+        else:
+            try:
+                with self.con:
+                    with self.con.cursor() as cur:
+                        cur.execute("SELECT count(*) from ITEMS;")
+            except InterfaceError:
+                # con.closed is 0 but still unable to connect to database
+                # sendToAdmin("con.closed fails to check properly")
+                self.con = connect(self.dbFile)
 
-    def addItem(self,user,link):
+
+
+    def addItem(self,user,_link):
         try:
             self.connectToDb()
-            if 'amazon' in link or 'amzn' in link:
-                bot.send_message(user,"Currently this bot doesn't support Amazon links.")
+            # if 'amazon' in _link or 'amzn' in _link:
+            if 'flipkart' not in _link:
+                bot.send_message(user,"Currently this bot only supports Flipkart links.")
                 return
+
             message = bot.send_message(user, "Searching for the product🧐. Please wait.")
+            links = re.findall("(?P<url>https?://[^\s]+)",_link)
+            if len(links) ==0:
+                bot.send_message(user,"No links found.")
+                return
+            elif len(links)>1:
+                bot.send_message(user,"Only one link is supported at a time.")
+                return
+
+            link = links[0]
+
             with self.con:
                 with self.con.cursor() as cur:
                     cur.execute("SELECT count(*) from ITEMS where link=%s",(link,))
@@ -62,11 +88,13 @@ class DataBase:
                         
                         newValues = queryPrice([link])
                         if newValues:
-                            name,price = newValues[0]
+                            n,p = newValues[0]
                             cur.execute(
                                 'INSERT into ITEMS (userId, link, name,addedPrice, price) values (%s,%s,%s,%s,%s)',
-                                (user,link,name,price,price))
-                            bot.send_message(user,f'The following product is added for tracking.\n<i>{name}</i> \nCurrent Price: <b>{price}</b>')
+                                (user,link,n,p,p))
+
+                            bot.send_message(user,
+                              f'The following product(s) are added for tracking.\n\n<i>{n}</i> \nCurrent Price: <b>{p}</b>')
                         else:
                             bot.send_message(user,'Unable to find the product.')
 
@@ -76,10 +104,10 @@ class DataBase:
             bot.send_message(user,'Unable to find the product. Check if the link is ok.')
         except:
             print(traceback.format_exc())
+            sendToAdmin("exception occurred in adding new item")
             bot.send_message(user,'Unable to find the product.')
         finally:
             bot.delete_message(message.chat.id, message.message_id)
-
 
 
 
@@ -100,21 +128,21 @@ class DataBase:
 
                         for l,(_,p) in zip(links,newValues):
                             cur.execute("UPDATE ITEMS SET price=%s where link=%s",(p,l))
-                        cur.execute('SELECT name, price, addedPrice,link from ITEMS where userId=%s',(user,))
-                        txt = self.buildList(cur.fetchall())
+                        cur.execute('SELECT name, price, addedPrice,link from ITEMS where userId=%s ORDER BY itemID',(user,))
+                        txt = f"Here is your current product list.\n<b>{'-'*50}</b>\n\n" + self.buildList(cur.fetchall())
                         bot.send_message(user,txt,disable_web_page_preview=True)
                     else:
                         bot.send_message(user,'Unable to get update for some product')
         except:
             print(traceback.format_exc())
+            sendToAdmin("exception occurred during update")
         finally:
             bot.delete_message(message.chat.id, message.message_id)
 
 
+
     def scheduleUpdate(self):
         toUpdate = []
-        print("Schedule update trigger.")
-        bot.send_message(ADMIN,"Schedule update trigger.")
         try:
             self.connectToDb()
             with self.con:
@@ -122,13 +150,16 @@ class DataBase:
                     cur.execute("SELECT userid from ITEMS group by userid")
                     userids = cur.fetchall()
                     for u in userids:
-                        cur.execute("SELECT link,price from ITEMS where userid=?",u)
+                        cur.execute("SELECT link,price from ITEMS where userid=%s",u)
                         infos = cur.fetchall()
                         links, oPrice = zip(*infos)
                         newValues = queryPrice(links)
-                        update = [[p,l] for l,(_,p) in zip(links,newValues)]
-                        print(update)
-                        cur.executemany("UPDATE ITEMS SET price=? where link=?",update)
+                        update = [[p,l] for l,(_,p) in zip(links,newValues) if p ]
+                        # print(update)
+                        noUpdate = [l for l,(_,p) in zip(links,newValues) if not p ]
+                        if len(noUpdate):
+                            sendToAdmin("Cound not update these links\n" +"\n".join(noUpdate))
+                        cur.executemany("UPDATE ITEMS SET price=%s where link=%s",update)
                         
                         # check if price of any product is dropped
                         for oP,(_,nP) in zip(oPrice,newValues):
@@ -141,21 +172,20 @@ class DataBase:
                 with self.con.cursor() as cur:
                     for u in toUpdate:
                         print(f"Price dropped for items for user {u[0]}")
-                        cur.execute('SELECT name, price, addedPrice, link from ITEMS where userId=?',u)
+                        cur.execute('SELECT name, price, addedPrice, link from ITEMS where userId=%s',u)
                         txt = f"<b>Price dropped for some items in your list.</b>\n<b>{'-'*50}</b>\n\n" + self.buildList(cur.fetchall())
                         bot.send_message(u[0],txt,disable_web_page_preview=True)
+            # sendToAdmin('Scheduled update done.')
         except:
             print(traceback.format_exc())
-
-
+            sendToAdmin("exception occurred in schedule update")
 
 
 
 
     def buildList(self,info):
         # info is a list fo title, price and added price
-        txt = "Here is your current product list.\n"
-        txt += f"<b>{'-'*50}</b>\n\n"
+        txt = ""
         for i,(title,price,addedPrice,link) in enumerate(info):
             txt += f"{i+1}. <a href='{link}'><i>{title}</i></a>\nPrice: <b>{price}</b>"
             if price-addedPrice>0:
@@ -173,7 +203,7 @@ class DataBase:
             if len(prompt)==0: # send the list
                 with self.con:
                     with self.con.cursor() as cur:
-                        cur.execute('SELECT itemID,name from ITEMS where userId=%s',(user,))
+                        cur.execute('SELECT itemID,name from ITEMS where userId=%s ORDER BY itemID',(user,))
                         values = cur.fetchall()
                         if len(values) == 0:
                             bot.send_message(user,'No items found on list')
@@ -191,6 +221,7 @@ class DataBase:
                         bot.send_message(user, f"Your product <i>{name}</i> is removed from list.")
         except:
             print(traceback.format_exc())
+            sendToAdmin("exception occurred during untrack")
 
     def listAll(self):
         try:
@@ -202,6 +233,7 @@ class DataBase:
                     bot.send_message(ADMIN,txt)
         except:
             print(traceback.format_exc())
+            sendToAdmin("exception occurred in status")
         
 
 
@@ -216,16 +248,27 @@ db = DataBase()
 
 async def check_price(session:ClientSession, url:str):
     async with session.get(url) as resp:
-        print("checking price for ",url)
+        # print("checking price for ",url)
         page = await resp.read()
         soup = BeautifulSoup(page, 'html.parser')
         if 'flipkart' in url: # if flipkart
-            title = soup.find("span", {"class": "B_NuCI"}).get_text()
-            price = soup.find("div", {"class": "_30jeq3 _16Jk6d"}).get_text()[1:].replace(',','')
+            title = soup.find("span", {"class": "B_NuCI"})
+            if title :
+                title = title.get_text().strip()
+            else:
+                return None,None
+            company = soup.find("span", {"class": "G6XhRU"})
+            company = company.get_text().strip() if company else ""
+            price = soup.find("div", {"class": "_30jeq3 _16Jk6d"})
+            if price:
+                price = price.get_text()[1:].replace(',','')
+            else:
+                return None, None
             print('checking price',title,price)
-            return title,price #prints the price
-        else:
-            return None
+            fullTitle = f"{company} {title}"
+            return fullTitle,price #prints the price
+        # else:
+        #     return None,None
         # elif 'amazon' in url or 'amzn' in url:# for amazon
         #     title = soup.find("span", {"id": "productTitle"}).get_text()
         #     price = soup.find("span", {"class": "a-offscreen"}).get_text()[1:].replace(',','')
@@ -261,6 +304,7 @@ def check_price_flipkart(url:str):
             print('Unknown website')
     except:
         print(traceback.format_exc())
+        sendToAdmin("exception occurred")
 
 
 
@@ -290,7 +334,7 @@ def newLink(message):
     elif link.startswith('/untrack') : 
         # pass as /untrack 5,6 to untrack them
         db.untrack(user,link)
-    elif link.lower()=='list' and int(user) == int(ADMIN):
+    elif link.lower()=='status' and int(user) == int(ADMIN):
         db.listAll()
     else:
         print('link received')
@@ -320,11 +364,18 @@ def webhook():
     </div>''', 200
 
 
+class RepeatTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
+
+def sendToAdmin(msg:str):
+    bot.send_message(ADMIN,msg)
 
 
 if __name__ == "__main__":
-    bot.send_message(ADMIN,'Bot Started.')
-    t = Timer(3600, db.scheduleUpdate)
-    t.start()
+    sendToAdmin("Bot started")
+    t = RepeatTimer(3600, db.scheduleUpdate).start()
     from waitress import serve
     serve(server, host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
